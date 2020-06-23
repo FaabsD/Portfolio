334b905458aab8cc7ff95c08d80f2ed729bae486
Fabian Hendriks <19959@ma-web.nl>
2020-06-23 15:32:19 +0200

    vierde item ingevoerd

--- private/views/homepage.php
+++ private/views/homepage.php
@@ -84,6 +84,26 @@
 				</div>
 			</div>
 		</div>
+		<div class="container-item" data-item="item4">
+			<img src="<?php echo site_url('/images/image-frame-2.png')?>" alt="">
+			<h2>Covid-19 hulptool</h2>
+			<p>
+				Weer een project in samenwerkings verband. Dit keer moest je als team vanwege de Corona crisis een hulptool ontwikkelen.
+				<span class="dots">...</span><br>
+				<span class="more">
+					Voor dit project hadden wij als team de volledige vrijheid om iets te maken wat de mensen zou helpen in deze crisis.<br>
+					Wij kozen ervoor om een website te maken waarop mensen kunnen kijken wat de druktes in hun lokale winkels zijn. 
+					Men kan de winkel opzoeken en dan vervolgens aangeven hoe druk het is. Als hun winkel er niet tussen staat, 
+					dan kunnen ze een account aanmaken en inloggen om de ontbrekende winkel vervolgens zelf toe te voegen. 
+					Dit is ook het eerste project geweest waarin we hebben gewerkt in een MVC structuur<br>
+					Mijn taken in dit project bestonden uit: het maken van de zoekfunctie(PHP), 
+					Het maken en afhandelen van het registratie formulier(HTML, CSS, PHP), 
+					Het toevoegen van een winkel(PHP) en het aangeven van de drukte(PHP).<br>
+					Dit project is gemaakt in: <span class="HTML">HTML</span>, <span class="CSS">CSS</span> & <span class="PHP">PHP</span>
+				</span>
+			</p>
+			<button onclick="readMoreLess('item4')" class="read">Lees meer</button>
+		</div>
 	</div>
 </a>
 <?php $this->stop(); ?>
