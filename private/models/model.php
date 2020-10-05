b958dc34d3678cb9b335aa3a33759553115f0c20
Fabian Hendriks <19959@ma-web.nl>
2020-10-05 16:20:30 +0200

    functie gemaakt die één project uit de database ophaalt gebasseerd op zij project_id

--- private/models/model.php
+++ private/models/model.php
@@ -2,10 +2,21 @@
 // Model functions
 // In dit bestand zet je ALLE functions die iets met data of de database doen
 
-function getProjects() {
-	$connection = dbConnect();
-	$sql        = "SELECT * FROM `projects`";
-	$statement  = $connection->query( $sql );
+function getProjects()
+{
+    $connection = dbConnect();
+    $sql = "SELECT * FROM `projects`";
+    $statement = $connection->query($sql);
 
-	return $statement->fetchAll();
+    return $statement->fetchAll();
+}
+
+function getProject($id)
+{
+    $connection = dbConnect();
+    $sql = "SELECT * FROM `projects` WHERE `project_id` = :id";
+    $statement = $connection->prepare($sql);
+    $statement->execute(['id' => $id]);
+
+    return $statement->fetchAll();
 }
